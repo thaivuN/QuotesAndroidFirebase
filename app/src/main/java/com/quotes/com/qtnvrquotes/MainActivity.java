a8f318171a5c2914c748aec0cd9be4a32b0e7f3d
Thai-Vu Nguyen <thaivu22@gmail.com>
2016-10-27 10:47:14 -0400

    Update

--- app/src/main/java/com/quotes/com/qtnvrquotes/MainActivity.java
+++ app/src/main/java/com/quotes/com/qtnvrquotes/MainActivity.java
@@ -3,12 +3,24 @@ package com.quotes.com.qtnvrquotes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends BaseActivity {
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private FirebaseDatabase mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+
     }
 
 
